42d091cbcbfaba7cfc05c2920519b502171906d8
Ryan Tharp <neuro@interx.net>
2019-05-31 18:06:00 -0700

    use new refactor api

--- client.js
+++ client.js
@@ -1,5 +1,11 @@
 const networkTest = require('./lib.networkTest')
 
-networkTest.testUpload('127.0.0.1', 3000, function(results) {
-  console.log('FINAL (remote) RESULTS: bytes per second:', networkTest.formatBytes(results.uploadBytesPerSec), 'from', results.ip)
+networkTest.createClient('127.0.0.1', 3000, function(client) {
+  client.testPort(3001, function(results) {
+    console.log('port test complete', results)
+    client.testUpload(function(results) {
+      console.log('FINAL (remote) RESULTS: bytes per second:', networkTest.formatBytes(results.uploadBytesPerSec), 'from', results.ip)
+      client.disconnect()
+    })
+  })
 })
